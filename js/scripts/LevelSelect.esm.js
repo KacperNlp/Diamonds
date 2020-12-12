11f87fbde56123ac457fea58ec1c1ec16f9b8b03
Kacper Nalepa <kacper.nlp@wp.pl>
2020-12-12 21:05:57 +0100

    Created class Canvas to drawing game

--- js/scripts/LevelSelect.esm.js
+++ js/scripts/LevelSelect.esm.js
@@ -1,13 +1,41 @@
-import {Common} from './Common.esm.js';
+import {Common, HIDDEN_SCREEN, VISIBLE_SCREEN} from './Common.esm.js';
+import {canvas} from './Canvas.esm.js'
 
 const LEVEL_SELECT_ID = 'js-level-select-screen';
 const LEVEL_SELECT_BUTTON_CLASS = 'level-select__button';
 
+const buttons = [{
+    level: 1,
+}]
+
 class LevelSelect extends Common{
     constructor(){
         super(LEVEL_SELECT_ID);
+
+        buttons.forEach(gameLevel => this.createButton(gameLevel.level))
     }
 
+    #canvas = canvas;
+
+    createButton(value){
+        const button = document.createElement('button');
+
+        button.type= button;
+        button.classList.add('level-select__button');
+        button.textContent = value;
+        button.value = value;
+
+        button.addEventListener('click', this.buttonOnClickHandler)
+
+        this.element.appendChild(button);
+    }
+
+    buttonOnClickHandler=(event)=>{
+        event.preventDefault();
+
+        this.changeVisibilityOfScreen(this.element, HIDDEN_SCREEN);
+        this.changeVisibilityOfScreen(this.#canvas.element, VISIBLE_SCREEN);
+    }
 }
 
 export const levelSelect = new LevelSelect();
