14fb6a24889af590e2223fd4b93f7267d3d2d5ec
Kacper Nalepa <kacper.nlp@wp.pl>
2020-12-19 22:17:27 +0100

    creating Settings to show the settings screen

---
+++ js/scripts/Settings.esm.js
@@ -0,0 +1,23 @@
+import {Common} from './Common.esm.js'
+
+
+const SETTINGS_SCREEN_ID ='js-settings-screen';
+//game sounds
+const GAME_SOUNDS_ON_OFF_BUTTON_ID = 'js-game-sounds';
+const GAME_SOUNDS_INCREASE_BUTTON_ID ='js-game-sounds-increase';
+const GAME_COUNDS_DECREASE_BUTTON_ID = 'js-game-sounds-decrease';
+//music
+const GAME_MUSIC_ON_OFF_BUTTON_ID = 'js-music';
+const GAME_MUSIC_INCEASE_BUTTON_ID = 'js-musice-increase';
+const GAME_MUSIC_DECREASE_BUTTON_ID = 'js-music-decrease';
+//exite button
+const EXITE_SETTINGS_BUTTON_ID = 'js-settings-exite';
+
+
+class Settings extends Common{
+    constructor(){
+        super(SETTINGS_SCREEN_ID);
+    }
+}
+
+export const settings = new Settings();
